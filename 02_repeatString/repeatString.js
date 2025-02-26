390c36e4bce4fe80bbdc7fb92cb91eacf796a59c
eggfryed <[200~146197827+eggfryed@users.noreply.github.com~>
2025-02-26 13:58:15 +0000

    completed task
    string repeats three times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(greeting, times) {
+    if (times < 0)
+        return "more greetings please";
 
+    let greet = "hey";
+    
+    for (i = 1; i < times; i++) {
+        greet += greeting;
+    }
+
+    return greet;
 };
 
-// Do not edit below this line
+// Do not edit below this lineS
 module.exports = repeatString;
