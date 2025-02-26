206c1feebbd7150299d457b37f0b3874b8bb6c2e
eggfryed <[200~146197827+eggfryed@users.noreply.github.com~>
2025-02-26 14:35:05 +0000

    reverse string "hello"

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+const reverseString = function(original, reversed) {
+//message
+original = "hello";
+
+//split message
+let splitMessage = original.split();
+
+//reverse message
+let reverseMessage = splitMessage.reverse();
+
+//join message
+let joinMessage = reverseMessage.join();
+
+//return new message
+return joinMessage; 
 
 };
+
+console.log (reverseString());
 
 // Do not edit below this line
 module.exports = reverseString;
