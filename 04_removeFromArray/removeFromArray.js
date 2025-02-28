12247e81a7fb69bbc11107ada5149fdd9207176f
eggfryed <[200~146197827+eggfryed@users.noreply.github.com~>
2025-02-28 19:02:35 +0000

    remove a single value from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, toRemove) {
+
+  //find the position of the number to remove
+  index = array.indexOf(toRemove);
+  console.log (index);
+
+  //remove number from array
+  newArray = array.splice(index, 1);
+  console.log (newArray);
+
+  //return new array
+  return (array);
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
