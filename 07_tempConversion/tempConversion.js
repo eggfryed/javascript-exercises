fb13e199b18b51bb91b9bea20d25fb61d88a38c7
eggfryed <[200~146197827+eggfryed@users.noreply.github.com~>
2025-03-02 10:59:12 +0000

    convert temp to celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  //conversion
+  const celsius = ((f-32)*(5/9));
+
+  console.log (celsius);
+
+  //return
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  //conversion
+  const fahrenheit = (c*((9/5)+32));
+
+  console.log (fahrenheit);
+
+  //return
+  return fahrenheit;
 };
 
 // Do not edit below this line
