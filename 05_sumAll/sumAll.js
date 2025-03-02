80d1b9cfb5f280ba58de249fa6862c2de28e8b4d
eggfryed <[200~146197827+eggfryed@users.noreply.github.com~>
2025-03-02 07:19:17 +0000

    sum all numbers within range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    //value of result
+    let result = 0
 
-};
+    //define num values
+    let maxNum = Math.max(num1, num2);
+    let minNum = Math.min(num1, num2);
 
+    //parameter values
+    console.log (minNum);
+    console.log (maxNum);
+    
+    //iterable loop
+    for (let i = minNum; i <= maxNum ; i++) {
+        result += i;
+    }
+
+    //return result
+    return result;
+}
 // Do not edit below this line
 module.exports = sumAll;
