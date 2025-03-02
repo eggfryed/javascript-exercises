cb44f2e72017d9a604ef159303f4990d24f0f394
eggfryed <[200~146197827+eggfryed@users.noreply.github.com~>
2025-03-02 08:57:42 +0000

    works with other type of century years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,12 @@
 const leapYears = function(year) {
 
-console.log(year);
-
-//leap years are divisible by four
-if ((year % 4 != 0) || (year % 100 == 0)) {
-    return false;
-} else return true;
-};
-
-// Do not edit below this line
-module.exports = leapYears;
+    console.log(year);
+    
+    //leap years are divisible by four
+    if ((year % 4 != 0) || (year % 400 != 0) && (year % 100 == 0)) {
+        return false;
+    } else return true;
+    };
+    
+    // Do not edit below this line
+    module.exports = leapYears;
